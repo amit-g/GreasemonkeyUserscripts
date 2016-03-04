248e0cbcefac4dfe1b3a035549715827b102c23a
Amit G <amitg.github@yahoo.com>
2016-03-04 15:55:15 -0800

    Added Jira

---
+++ Jira/Jira.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        Jira
+// @namespace   https://ipayment.atlassian.net/browse/
+// @description Changes the changeset to a link
+// @include     /^https://ipayment.atlassian.net/browse/[A-Z]+-[0-9]+$/
+// @version     1
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
+// @grant       none
+// ==/UserScript==
+
+(function () {
+	run();
+ 
+	function run() {
+		detectChangesetsAndMakeTfsLink();
+	}
+	
+	function detectChangesetsAndMakeTfsLink() {
+    var baseTfsLink = "iProject:8080/tfs/main/_versionControl/changeset?id=";
+    
+    $(".activity-comment .action-body").each(function(){
+      var html = $(this).html();
+      if (html.indexOf("Changeset") != -1) {
+        var re = /(Changeset[s: ]*)([0-9]+)/ig;
+        html = html.replace(re, "<a href='" + baseTfsLink + "$2'>$1 $2</a>");
+        $(this).html(html)
+      }
+    });
+	}
+})();
---
+++ Jira/README.md
@@ -0,0 +1,4 @@
+Github Run
+==========
+
+This is a Greasemonkey Userscript to convert the changeset in comments to a TFS changeset link.
