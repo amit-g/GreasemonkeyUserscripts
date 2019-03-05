4623f991e6d7a4f2a3e800c46c6eda11268fa34c
amit-g <amit-g@users.noreply.github.com>
2019-03-04 16:09:52 -0800

    Updated the script to make slightly more resilient

--- YoutubeWhitelist/YouTubeWhiteList.user.js
+++ YoutubeWhitelist/YouTubeWhiteList.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Allow only whitelisted videos on Youtube
-// @author       Amit
+// @author       Amit G
 // @namespace    https://github.com/amit_g
 // @description  Allow only whitelisted videos on Youtube
 // @version      1.0.0
-// @match        *://www.youtube.com/watch*
+// @match        *://www.youtube.com/*
 // @exclude      *://www.youtube.com/embed/*
-// @grant        GM.xmlHttpRequest 
+// @connect      gist.githubusercontent.com
+// @grant        GM.xmlHttpRequest
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @require      https://code.jquery.com/jquery-3.2.1.slim.min.js     
+// @require      https://code.jquery.com/jquery-3.2.1.slim.min.js
 // ==/UserScript==
 
 /** DESCRIPTION
@@ -23,66 +24,143 @@
     /* Variables */
     var youTubeFilters;
     var owner;
+    var playerRemovalDecisionMade = false;
+    var intervalID;
+
+    console.log("Welcome to youTubeWhiteListed...");
 
     async function initValues() {
+        console.log("initValues...")
         GM.xmlHttpRequest({
             method: 'GET',
             url: 'https://gist.githubusercontent.com/amit-g/9888db1a54209f0e7223c32c451638e4/raw/edfbad904017e3ca671347607cb25f12f8d54dfb/YouTubeFilters.json',
             onload: function (response) {
                             youTubeFilters = JSON.parse(response.responseText);
-                            //console.log(youTubeFilters);
+                            console.log(youTubeFilters);
                         }
         });
+        console.log("initValues after...");
     }
 
     await initValues();
- 
+
+    /* INTERVAL FOR BLACKLISTING */
+
     performWhiteListChecks();
-    setInterval(performWhiteListChecks, 5000);
+    intervalID = setInterval(performWhiteListChecks, 5000);
 
     function performWhiteListChecks () {
+        console.warn('performWhiteListChecks');
         findOwner();
-        removePlayer();
-    }
- 
-    function findOwner() {
-        var $owner = $(ownerSelector);
-        if ($owner.length) {
-            owner = $owner.text();
+        checkAndReplacePlayer();
+
+        if (playerRemovalDecisionMade && intervalID) {
+            console.log('removal decision made. Clearing the interval for performWhiteListChecks');
+            clearInterval(intervalID);
         }
     }
 
-    function removePlayer() {
-        if (!shouldRemoveVideoPlayer()) {
+    function findOwner() {
+        var $owner = $(ownerSelector);
+
+        if ($owner.length) {
+            owner = $owner.text();
+            console.log("owner Found...");
+        }
+        else {
+            console.log("owner not Found...");
+        }
+    }
+
+    function checkAndReplacePlayer() {
+        console.log("removePlayer...");
+
+        if (!playerRemovalDecisionCanBeMade()) {
+            console.log("returning from removePlayer as not all data is loaded...");
+
             return;
         }
 
-        $player = $(videoPlayerSelector);
+        playerRemovalDecisionMade = true;
+
+        if (!shouldRemoveVideoPlayer()) {
+            console.log("returning from removePlayer as it is whitelisted...");
+
+            return;
+        }
+
+        playerRemovalDecisionMade = replacePlayer();
+    }
+
+    function replacePlayer() {
+        console.log("replacePlayer...");
+
+        var $player = $(videoPlayerSelector);
 
         if ($player.length) {
-            $player.remove();
+            console.log("replacing Player...");
+            console.log($player);
+
+            var imageUrl = getPlaceholderImage($player);
+            var imageTag = "<image src='" + imageUrl + "'>";
+
+            $player.replaceWith(imageTag);
+
+            return true;
         }
+        else {
+            console.log("Player not replaced...");
+
+            return false;
+        }
+
+        return false;
+    }
+
+    function getPlaceholderImage($player) {
+        var defaultWidth = 948;
+        var defaultHeight = 533;
+        var url = "https://via.placeholder.com/" + defaultWidth + "x" + defaultHeight + ".png/333333/CCCCCC?text=Not%20Allowed.%20Think%20it%20should%20be?%20Send%20A%20Pull%20Request.";
+        var width = $player.clientWidth || defaultWidth;
+        var height = $player.clientHeight || defaultHeight;
+
+        url = url.replace("defaultWidth", width);
+        url = url.replace("defaultHeight", height);
+
+        return url;
+    }
+
+    function playerRemovalDecisionCanBeMade() {
+        return areFiltersLoaded() && isOwnerFound();
     }
 
     function shouldRemoveVideoPlayer() {
-        return areFiltersLoaded() && isOwnerFound() && !isOwnerWhilelisted();
+        return playerRemovalDecisionCanBeMade() && !isOwnerWhilelisted();
     }
 
     function areFiltersLoaded() {
+        console.log('areFiltersLoaded: ' + !!youTubeFilters)
         return !!youTubeFilters;
     }
 
     function isOwnerFound() {
+        console.log('owner: ' + !!owner)
         return !!owner;
     }
 
     function isOwnerWhilelisted() {
+        console.log('isOwnerWhilelisted: ');
+        console.log(youTubeFilters);
+        console.log(youTubeFilters.whitelist);
+        console.log(youTubeFilters.whitelist.owners);
         if (youTubeFilters && youTubeFilters.whitelist && youTubeFilters.whitelist.owners) {
+            console.log('youTubeFilters.whitelist.owners.includes(owner): ' + youTubeFilters.whitelist.owners.includes(owner));
             return youTubeFilters.whitelist.owners.includes(owner);
         }
         else {
+            console.log('youTubeFilters.whitelist.owners.includes(owner): true is returned');
             return true;
         }
     }
- 
+
  })(jQuery);
